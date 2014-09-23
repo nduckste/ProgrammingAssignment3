662023b15312e0d807e541796de15bc6cc506835
Nick Duckstein <nduckste@gmail.com>
2014-09-23 01:28:38 -0700

    finished rankall.R

--- rankall.R
+++ rankall.R
@@ -1,4 +1,4 @@
-rankhospital <- function(state, outcome, num = "best") {
+rankall <- function(outcome, num = "best") {
     ## Read outcome data
     data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
 
@@ -14,55 +14,68 @@ rankhospital <- function(state, outcome, num = "best") {
         stop(errmsg)
     }
 
-    #create data.frame of unique states
-#     states <- data.frame(stateval=unique(data[,7]))
-    ## Check that state is valid, i.e. in data frame of unique states
-    if (length(data[data[,7]==state,7])==0) {
-#      checkstate <- subset(states,stateval==state)
-#      if (nrow(checkstate) == 0) {
-        errmsg <- "invalid state\n"
-        stop(errmsg)
-    }
-    
     #generate the column index associated with the outcome parameter    
     oi <- data.frame(outcomedesc = c("heart attack","heart failure","pneumonia"), index=c(11,17,23))
     oiindex <- oi[oi[,"outcomedesc"]==outcome,"index"]
 
-    #now pull hospitals for given state. Include the outcome column
-    h <- data[data[,7] == state,c(2,7,as.numeric(oiindex))]
-
+    #now pull hospitals, state, and outcome
+    h <- data[,c(2,7,as.numeric(oiindex))]
+    names(h) <- c("hospital","state","rate")
+    
     #convert outcome column to numeric, supressing warning messages
-    suppressWarnings(h[,3] <- as.numeric(h[,3]))
+    suppressWarnings(h[,"rate"] <- as.numeric(h[,"rate"]))
 
     #order data by outcome, hospital name, removing NAs
-    h <- h[order(h[,3],h[,1],na.last=NA),]
+    h <- h[order(h[,2],h[,3],h[,1],na.last=NA),]
 
-    
     #Return hospital name in that state with lowest 30-day death rate
     if (num=="best") {
-        #return the first row
-        h[1,1]
+        #Sor ascending
+        h <- h[order(h[,2],h[,3],h[,1],na.last=NA),]
     }
     else if (num=="worst"){
-        #return the last row
-        #tail(h[,1],1)
-        h[nrow(h),1]
+        #Sort descending
+        h <- h[order(h[,2],-h[,3],h[,1],na.last=NA),]
     }
     else {
-        #Turn num into an integer
-        num <- as.integer(num)
-        
-        #If num is greater than size of h return NA
-        if (nrow(h) < num) {
-            return(NA)
+        #Sor ascending
+        h <- h[order(h[,2],h[,3],h[,1],na.last=NA),]
+    }
+
+    #     create data.frame of unique states
+    states <- data.frame(stateval=unique(data[,7]))
+    states <- sort(states[,"stateval"])
+    for (s in states) {
+        #bring back top row where s == state
+        if (num=="best" || num=="worst") {
+            data_subset <- head(h[h[,2] == s,1:2],1)
         }
+        #bring back last row where s == state
+        else if (num=="worst") {
+            data_subset <- tail(h[h[,2] == s,1:2],1)
+        }
+        #bring back num row where s == state
+        else  {
+            data_subset <- h[h[,2] == s,1:2]
+            data_subset <- data_subset[num,]
+        }
+        
+        #assign s (current state) to the State column in case row was NA
+        data_subset[,"state"]=s
+        if (exists("d")) {
+            d <- rbind(d, data_subset)
+        }
+        #else dataset the first time, assinging data_subset to it.
         else {
-            #return the row equal to num
-            h[num,1]
+            d <- data_subset
         }
     }
+    d
 }
 
 # Rprof("best.out")
 # y <- best("TX","heart attack")
 # Rprof(NULL)
+# head(rankall("heart attack",20),10)
+# tail(rankall("pneumonia","worst"),3)
+# tail(rankall("heart failure"),10)
