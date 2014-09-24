016d88fef7166ccd32a28ce5fe8ce87cca28b4f9
Nick Duckstein <nduckste@gmail.com>
2014-09-24 13:11:40 -0700

    test of rankall without a loop

---
+++ rankall-noloop.R
@@ -0,0 +1,64 @@
+rankall <- function(outcome, num = "best") {
+    ## Read outcome data
+    data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+
+    ## Check that num  is valid
+    if (!(num == "best" || num == "worst" || is.numeric(num))) {
+        errmsg <- "invalid num. Valid values are 'best','worst', or an integer.\n"
+        stop(errmsg)
+    }
+    
+    ## Check that outcome is valid
+    if (outcome != "heart attack" && outcome != "heart failure" && outcome != "pneumonia") {
+        errmsg <- "invalid outcome\n"
+        stop(errmsg)
+    }
+
+    #generate the column index associated with the outcome parameter    
+    oi <- data.frame(outcomedesc = c("heart attack","heart failure","pneumonia"), index=c(11,17,23))
+    oiindex <- oi[oi[,"outcomedesc"]==outcome,"index"]
+
+    #now pull hospitals, state, and outcome
+    h <- data[,c(2,7,as.numeric(oiindex))]
+    names(h) <- c("hospital","state","rate")
+    
+    #convert outcome column to numeric, supressing warning messages
+    suppressWarnings(h[,"rate"] <- as.numeric(h[,"rate"]))
+
+    #order data by outcome, hospital name, removing NAs
+    h <- h[order(h[,2],h[,3],h[,1],na.last=NA),]
+
+    #Return hospital name in that state with lowest 30-day death rate
+    if (num=="best") {
+        #Sor ascending
+        h <- h[order(h[,2],h[,3],h[,1],na.last=NA),]
+    }
+    else if (num=="worst"){
+        #Sort descending
+        h <- h[order(h[,2],-h[,3],h[,1],na.last=NA),]
+    }
+    else {
+        #Sor ascending
+        h <- h[order(h[,2],h[,3],h[,1],na.last=NA),]
+    }
+
+    #     create data.frame of unique states
+    states <- sort(unique(h[,2]))
+    
+    do.call("rbind", as.list(
+        by(h, list(state=h$state), function(x){
+            #y <- x[nrow(x),1:2] # worst
+            y <- x[1,1:2] # best
+            apply(y, 2, head)
+        }
+            )
+        )
+    )
+}
+
+# Rprof("best.out")
+# y <- best("TX","heart attack")
+# Rprof(NULL)
+# head(rankall("heart attack",20),10)
+# tail(rankall("pneumonia","worst"),3)
+# tail(rankall("heart failure"),10)
