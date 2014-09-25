5c7e30734881267fd010231b4cb9df775b68cf21
Nick Duckstein <nduckste@gmail.com>
2014-09-24 18:34:04 -0700

    finished optimized rankall-noloop.R

--- rankall-noloop.R
+++ rankall-noloop.R
@@ -1,7 +1,7 @@
 rankall <- function(outcome, num = "best") {
     ## Read outcome data
     data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-
+    
     ## Check that num  is valid
     if (!(num == "best" || num == "worst" || is.numeric(num))) {
         errmsg <- "invalid num. Valid values are 'best','worst', or an integer.\n"
@@ -13,45 +13,53 @@ rankall <- function(outcome, num = "best") {
         errmsg <- "invalid outcome\n"
         stop(errmsg)
     }
-
+    
     #generate the column index associated with the outcome parameter    
     oi <- data.frame(outcomedesc = c("heart attack","heart failure","pneumonia"), index=c(11,17,23))
     oiindex <- oi[oi[,"outcomedesc"]==outcome,"index"]
-
+    
     #now pull hospitals, state, and outcome
     h <- data[,c(2,7,as.numeric(oiindex))]
     names(h) <- c("hospital","state","rate")
     
     #convert outcome column to numeric, supressing warning messages
     suppressWarnings(h[,"rate"] <- as.numeric(h[,"rate"]))
-
-    #order data by outcome, hospital name, removing NAs
-    h <- h[order(h[,2],h[,3],h[,1],na.last=NA),]
-
+    
     #Return hospital name in that state with lowest 30-day death rate
     if (num=="best") {
-        #Sor ascending
-        h <- h[order(h[,2],h[,3],h[,1],na.last=NA),]
+        #Sort ascending
+        #order data  by outcome, hospital name, removing NAs
+        h <- h[order(h[,"state"],h[,"rate"],h[,"hospital"],na.last=NA),]
+        rowindex <- 1
     }
     else if (num=="worst"){
         #Sort descending
-        h <- h[order(h[,2],-h[,3],h[,1],na.last=NA),]
+        #order data  by outcome, hospital name, removing NAs
+        h <- h[order(h[,"state"],-h[,"rate"],h[,"hospital"],na.last=NA),]
+        rowindex <- 1
     }
     else {
-        #Sor ascending
-        h <- h[order(h[,2],h[,3],h[,1],na.last=NA),]
+        #Sort ascending
+        #order data  by outcome, hospital name, removing NAs
+        h <- h[order(h[,"state"],h[,"rate"],h[,"hospital"],na.last=NA),]
+        rowindex <- 1
+        
+        #Turn num into an integer
+        rowindex <- as.integer(num)
+        
     }
-
-    #     create data.frame of unique states
-    states <- sort(unique(h[,2]))
     
-    do.call("rbind", as.list(
-        by(h, list(state=h$state), function(x){
-            #y <- x[nrow(x),1:2] # worst
-            y <- x[1,1:2] # best
-            apply(y, 2, head)
-        }
+    as.data.frame(
+        #This generates a matrix
+        mat <- do.call("rbind", as.list(
+            by(h, list(state=h$state), function(x){
+                #return a data.frame
+                #                 col1 = name of hospital associated with rowindex 
+                #                 col2 = name of the first state
+                data.frame(hospital=x[rowindex,"hospital"], state=x[1,"state"])
+            }
             )
+        )
         )
     )
 }
